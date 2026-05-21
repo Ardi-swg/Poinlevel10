2563908d34717ac2bf1de9ff401badec73d0656f
Ardi-swg <email_github_muhammadsuhardi16@gmail.com>
2026-05-22 05:48:53 +0700

    upgrade script level 11 warna warni

--- script.sh
+++ script.sh
@@ -1,7 +1,13 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/bin/bash
 
-echo "=== Script Level 10 Jalan ==="
-date
-echo "User: $(whoami)"
-echo "Folder: $(pwd)"
-echo "Sukses bro!"
+# Kode warna
+MERAH='\033[0;31m'
+IJO='\033[0;32m'
+KUNING='\033[1;33m'
+RESET='\033[0m'
+
+echo -e "${MERAH}=== LEVEL 11 CLEAR ===${RESET}"
+echo -e "${IJO}Script ini udah di-upgrade bro!${RESET}"
+echo -e "${KUNING}Tanggal sekarang: $(date)${RESET}"
+echo -e "${IJO}User: $(whoami)${RESET}"
+echo -e "${MERAH}GG LU BRO!${RESET}"
