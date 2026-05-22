2977b8349be493e87b3eff73ba74885c26fc9be3
Ardi-swg <email_github_muhammadsuhardi16@gmail.com>
2026-05-22 19:01:34 +0700

    LEVEL 11 HIJAU NEON ☠️💚

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Kode warna
-MERAH='\033[0;31m'
+HIJAU_NEON='\033[1;92m'
 IJO='\033[0;32m'
 KUNING='\033[1;33m'
 RESET='\033[0m'
 
-echo -e "${MERAH}=== LEVEL 11 CLEAR ===${RESET}"
+echo -e "${HIJAU_NEON}=== LEVEL 11 CLEAR ===${RESET}"
 echo -e "${IJO}Script ini udah di-upgrade bro!${RESET}"
 echo -e "${KUNING}Tanggal sekarang: $(date)${RESET}"
 echo -e "${IJO}User: $(whoami)${RESET}"
-echo -e "${MERAH}GG LU BRO!${RESET}"
+echo -e "${HIJAU_NEON}GG LU BRO!${RESET}"
